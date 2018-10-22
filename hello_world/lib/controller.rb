51e9010fc828aff349ee8657a80c5cad5befabe8
jerome.delfosse <jerome.delfosse.78@gmail.com>
2018-10-22 20:14:47 +0200

    premiere commit

---
+++ hello_world/lib/controller.rb
@@ -0,0 +1,24 @@
+Bundler.require
+require 'gossip'
+require 'Bundler'
+
+class ApplicationController < Sinatra::Base
+	
+	get '/' do
+		erb :index, locals: {gossips: Gossip.all}
+	end
+
+	get '/gossips/new/' do
+	  erb :new_gossip
+	end
+
+	post '/gossips/new/' do
+		Gossip.new(params[:gossip_author], params[:gossip_content]).save
+		redirect '/'
+	end
+
+	get '/gossips/:num' do
+		erb :profil, locals: {gossips: Gossip.find(params[:num]), id: params[:num]}
+	end
+
+end	
